26c0c1819eebd90deccb80c4cc7626656afe2b50
GamerBike39 <champidub@gmail.com>
2023-07-21 15:20:38 +0200

    change id for username

--- src/app/r/[slug]/edit/[id]/page.tsx
+++ src/app/r/[slug]/edit/[id]/page.tsx
@@ -4,60 +4,57 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 
-
 interface pageProps {
- params: {  
-    id: string
- }
+  params: {
+    id: string;
+  };
 }
 
-const page= async ({ params } : pageProps) => {
+const page = async ({ params }: pageProps) => {
+  const content = await db.post.findFirst({
+    where: {
+      id: params.id,
+    },
+  });
 
-    
-    const content = await db.post.findFirst({
-        where: {
-            id: params.id,
-        },
-        
-    });
+  const user = await getAuthSession();
 
-    const user = await getAuthSession();
-    
+  if (!content) {
+    return notFound();
+  }
 
-    if(!content) {
-        return notFound();
-    }
-  
-    if(content.authorId !== user?.user?.id) {
-        return notFound();
-    }  
+  if (content.authorId !== user?.user?.id) {
+    return notFound();
+  }
 
-  
   return (
-   <div className="flex flex-col items-start gap-6">
-       <div className="border-b border-gray-200 pb-5">
-            <div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
-                <h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">
-                    Update post
-                </h3>
-                <p className="ml-2 mt-1 truncate text-sm text-gray-500">
-                  {content?.authorId}
-                  -- {user?.user?.id}
-                </p>
-            </div>
-       </div>
-
-        {/* form */}
-        <EditEditor postId={content.id} title={content.title} content={content.content} subRedditId={content.subRedditId}/>
-
-        <div className="flex w-full justify-end">
-            <Button type="submit" className="w-full" form="subreddit-post-form">
-                Post
-            </Button>
+    <div className="flex flex-col items-start gap-6">
+      <div className="border-b border-gray-200 pb-5">
+        <div className="-ml-2 -mt-2 flex flex-wrap items-baseline">
+          <h3 className="ml-2 mt-2 text-base font-semibold leading-6 text-gray-900">
+            Update post
+          </h3>
+          <p className="ml-2 mt-1 truncate text-sm text-gray-500">
+            u/ {user?.user?.username}
+          </p>
         </div>
+      </div>
 
-   </div>
-   )
-}
+      {/* form */}
+      <EditEditor
+        postId={content.id}
+        title={content.title}
+        content={content.content}
+        subRedditId={content.subRedditId}
+      />
+
+      <div className="flex w-full justify-end">
+        <Button type="submit" className="w-full" form="subreddit-post-form">
+          Post
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default page;
